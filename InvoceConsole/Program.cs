47ad4aeca5e73b9548505cb52003cc25cadd0477
Георгий <georgij.tar@yandex.ru>
2021-10-18 20:25:03 +0300

    Структура проекта

---
+++ InvoceConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InvoceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
